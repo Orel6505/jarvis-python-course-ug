11ea7f275dd9fd7ebd8eab2d045b77aa294f2cf8
idanCohenZ <101664812+idanCohenZ@users.noreply.github.com>
2025-01-28 12:32:10 +0200

    finished the menu class

--- jarvis.py
+++ jarvis.py
@@ -9,6 +9,40 @@ def input_validation(request, lowerbound, upperbound):
            return -1
 
 
+class Menu:
+       def __init__(self):
+              self.dict = {}
+       
+       def call_function_by_name(func_name, *args, **kwargs):
+              if func_name :
+                     return functions[func_name](*args, **kwargs)  # Pass arguments dynamically
+              else:
+                     raise ValueError(f"Function '{func_name}' is not defined.")
+
+       
+       def load_options(self, path_name):
+              with open(path_name, 'r') as input_file:
+                     line_index = 0
+                     while(True): 
+                            line = input_file.readline()
+                            if not line:
+                                break
+                            result = [item.strip() for item in line.split(".") if item.strip()]
+                            self.dict[line_index] = (result[0],result[1])
+                            line_index+=1             
+              
+       def __str__(self):
+              return str(self.dict)
+              
+                     
+ 
+a = Menu()
+a.load_options("menu_options.txt")
+# print(a)
+
+
+
+
      
     
     
