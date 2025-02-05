73f895252788f3411792ea34e56e9ac3153a11fd
idanCohenZ <101664812+idanCohenZ@users.noreply.github.com>
2025-02-05 09:22:39 +0200

    Updated the main and the input validation function

--- jarvis.py
+++ jarvis.py
@@ -1,12 +1,21 @@
 import datetime as date
 from enum import Enum
-def input_validation(request, lowerbound =0, upperbound=0):
+
+def input_validation(request: str, lowerbound =0, upperbound=0):
+    """ The function gets a string input, if it's a digit in range, it returns the digit.
+    otherwise, if it's digit but it's not in range or a non digit string it will return -1. """
     try:
-        request = int(request)
-        while request  < lowerbound and request  > upperbound:
-                    request = input("please insert a number between", lowerbound, "and: ", upperbound)
-        return request
-    
+        if(request.isdigit()):
+                request = int(request)
+                if request < upperbound and request  > lowerbound :
+                       return request
+                else:
+                       print("The number specified specified is not in range.")
+                       return -1
+        else:
+              print("Invalid input.")
+              return -1               
+                
     except Exception as e:
            return -1
 
@@ -54,7 +63,7 @@ class Status(Enum):
        DONE = 2
 
 class Item():
-       creation_date = date.today()
+       # creation_date = date.today()
        __title = "empty"
        __status = Status.START
        __priority = Priority.LOW
@@ -76,17 +85,21 @@ class Item():
 
 
 
-a = Menu()
-function_dict = {
-       "editFile":editFile,
-}
-a.load_options("menu_options.txt")
-print(a)
-print("#"*15)
-b = input("please insert your choice for personal assistant ")
-res = int(input_validation(b,0, a.get_upper_bound()))
-if res != -1:
-       a.call_function_by_name(a.dict[res][1],function_dict[a.dict[res][1]], "hi")
+# a = Menu()
+# function_dict = {
+#        "editFile":editFile,
+# }
+# a.load_options("menu_options.txt")
+# print(a)
+# print("#"*15)
+flag = -1
+while(flag == -1):
+       flag = input("please insert your choice for personal assistant ")
+# res = int(input_validation(b,0, a.get_upper_bound()))
+       flag = input_validation(flag,0, 2)
+print(flag)
+# if res != -1:
+#        a.call_function_by_name(a.dict[res][1],function_dict[a.dict[res][1]], "hi")
 
 
 
